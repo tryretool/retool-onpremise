5dbe1066b439b059fe7a817257d2b37c522f84ac
Ben Baker <ben@retool.com>
2021-07-29 09:56:09 -0700

    bump docker-compose to 1.29 (#59)

--- get-docker-compose.sh
+++ get-docker-compose.sh
@@ -1,2 +1,2 @@
-sudo -E curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo -E curl -L https://github.com/docker/compose/releases/download/1.29.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
