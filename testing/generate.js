46f90e84f49db47d8556650fcd10e9b53b0cbb43
Moksh Jawa <mokshjawa@gmail.com>
2021-08-28 14:04:44 -0700

    undo comment

--- testing/generate.js
+++ testing/generate.js
@@ -25,8 +25,7 @@ async function globalSetup() {
   await page.fill('#password', '${password}')
   await page.click('.auth-button')
   await page.waitForSelector('.app-browser', {timeout: 180000}) // Wait three minutes for apps to sync
-  # TEMP
-  # await page.context().storageState({ path: 'state.json' })
+  await page.context().storageState({ path: 'state.json' })
   await browser.close()
 
   fs.mkdirSync(resultsDir)
