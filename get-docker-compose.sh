1f826a5e76032a13f0fb70c1e7d999f423388317
Ben Baker <ben@retool.com>
2023-11-17 14:49:12 -0800

    [fix] Only be able to run install script once (#207)
    * only run install once
    
    * update copy

--- get-docker-compose.sh
+++ get-docker-compose.sh
@@ -1,2 +1,8 @@
+if command -v docker-compose &> /dev/null ; then
+    echo "docker-compose is already installed..."
+    echo "skipping docker-compose installation..."
+    exit 0
+fi
+
 sudo -E curl -L https://github.com/docker/compose/releases/download/1.29.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
--- get-docker.sh
+++ get-docker.sh
@@ -1,3 +1,9 @@
+if command -v docker &> /dev/null ; then
+    echo "docker is already installed..."
+    echo "skipping docker installation..."
+    exit 0
+fi
+
 # adapted from https://askubuntu.com/questions/459402/how-to-know-if-the-running-platform-is-ubuntu-or-centos-with-help-of-a-bash-scri
 command_present() {
   type "$1" >/dev/null 2>&1
